5dee2aed37ef2870ac30bec4ec73b3aadf561f1b
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-16 15:09:07 +0900

    フラグ引数を取得して表示するところまで

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # github_star_importer
 
-指定のGithubアカウントのStarを引き継ぐ
+指定のGitHubアカウントのStarを引き継ぐ
+
+```shell
+github_star_importer --from tune --to tunepolo --token "PERSONAL_ACCESS_TOKEN"
+```
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/urfave/cli"
@@ -12,6 +13,31 @@ func main() {
 	app.Name = "Github Star Importer"
 	app.Usage = "指定アカウントのStar情報を別アカウントにインポートする"
 	app.Version = "0.0.1"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "from, f",
+			Value: "tune",
+			Usage: "import FROM username",
+		},
+		cli.StringFlag{
+			Name:  "to, t",
+			Value: "tunepolo",
+			Usage: "import TO username",
+		},
+		cli.StringFlag{
+			Name:  "token",
+			Value: "PERSONAL_ACCESS_TOKEN",
+			Usage: "github personal access token for star import",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Printf("from: %+v\n", c.String("from"))
+		fmt.Printf("to: %+v\n", c.String("to"))
+		fmt.Printf("token: %+v\n", c.String("token"))
+		return nil
+	}
 
 	app.Run(os.Args)
 }
