b35b428532f4c2a9d9ea2c3088610e6e97378b25
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-18 21:14:34 +0900

    mainからcli.App生成部を分離

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ import (
 )
 
 func main() {
+	newApp().Run(os.Args)
+}
+
+func newApp() *cli.App {
 	app := cli.NewApp()
 
 	app.Name = "Github Star Importer"
@@ -71,5 +75,5 @@ func main() {
 		return nil
 	}
 
-	app.Run(os.Args)
+	return app
 }
