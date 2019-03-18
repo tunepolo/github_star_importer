81f7884941b4fbd880a02951ea1a861d1eddc8ea
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-18 21:23:07 +0900

    メイン処理を別関数化

--- main.go
+++ main.go
@@ -10,6 +10,60 @@ import (
 	"golang.org/x/oauth2"
 )
 
+var globalFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "from, f",
+		Value: "tune",
+		Usage: "import target username",
+	},
+	cli.StringFlag{
+		Name:  "token",
+		Value: "PERSONAL_ACCESS_TOKEN",
+		Usage: "github personal access token for star import",
+	},
+}
+
+func doImport(c *cli.Context) error {
+	fromUser := c.String("from")
+	token := c.String("token")
+
+	// トークンを使ってGitHubアクセスのためのclientを生成
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	// fromUserがStarをつけたリポジトリ一覧を取得
+	opt := &github.ActivityListStarredOptions{
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
+	var allRepositories []*github.StarredRepository
+	for {
+		repos, response, err := client.Activity.ListStarred(ctx, fromUser, opt)
+		if err != nil {
+			return err
+		}
+		allRepositories = append(allRepositories, repos...)
+		if response.NextPage == 0 {
+			break
+		}
+		opt.Page = response.NextPage
+	}
+
+	// 取得したリポジトリのリストを元にStarを付与
+	for _, repo := range allRepositories {
+		_, err := client.Activity.Star(ctx, repo.Repository.Owner.GetLogin(), repo.Repository.GetName())
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Starred : %s/%s\n", repo.Repository.Owner.GetLogin(), repo.Repository.GetName())
+	}
+
+	return nil
+}
+
 func main() {
 	newApp().Run(os.Args)
 }
@@ -21,59 +75,8 @@ func newApp() *cli.App {
 	app.Usage = "指定アカウントのStar情報を別アカウントにインポートする"
 	app.Version = "0.0.1"
 
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "from, f",
-			Value: "tune",
-			Usage: "import target username",
-		},
-		cli.StringFlag{
-			Name:  "token",
-			Value: "PERSONAL_ACCESS_TOKEN",
-			Usage: "github personal access token for star import",
-		},
-	}
-
-	app.Action = func(c *cli.Context) error {
-		fromUser := c.String("from")
-		token := c.String("token")
-
-		// トークンを使ってGitHubアクセスのためのclientを生成
-		ctx := context.Background()
-		ts := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: token},
-		)
-		tc := oauth2.NewClient(ctx, ts)
-		client := github.NewClient(tc)
-
-		// fromUserがStarをつけたリポジトリ一覧を取得
-		opt := &github.ActivityListStarredOptions{
-			ListOptions: github.ListOptions{PerPage: 100},
-		}
-		var allRepositories []*github.StarredRepository
-		for {
-			repos, response, err := client.Activity.ListStarred(ctx, fromUser, opt)
-			if err != nil {
-				return err
-			}
-			allRepositories = append(allRepositories, repos...)
-			if response.NextPage == 0 {
-				break
-			}
-			opt.Page = response.NextPage
-		}
-
-		// 取得したリポジトリのリストを元にStarを付与
-		for _, repo := range allRepositories {
-			_, err := client.Activity.Star(ctx, repo.Repository.Owner.GetLogin(), repo.Repository.GetName())
-			if err != nil {
-				return err
-			}
-			fmt.Printf("Starred : %s/%s\n", repo.Repository.Owner.GetLogin(), repo.Repository.GetName())
-		}
-
-		return nil
-	}
+	app.Flags = globalFlags
+	app.Action = doImport
 
 	return app
 }
