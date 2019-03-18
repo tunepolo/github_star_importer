e1f12fac5052b6668757ea1e97c13521676c0097
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-18 21:30:42 +0900

    作者・メール情報を書いてみた

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ func newApp() *cli.App {
 	app.Name = "Github Star Importer"
 	app.Usage = "指定アカウントのStar情報を別アカウントにインポートする"
 	app.Version = Version
-
+	app.Author = "Yuichi Tsunematsu"
+	app.Email = "tunepolo@gmail.com"
 	app.Flags = globalFlags
 	app.Action = doImport
 
